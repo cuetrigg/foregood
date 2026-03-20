dd6757c8efa2f06ee360517f78ebd2e151137c4f
cuetrigg <imaginedoingwork@gmail.com>
2026-03-21 01:19:54 +0200

    feat(images): allow remote avatar hosts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			new URL("https://forgood-cdn.azureedge.net/forgoodupload/**"),
+			new URL("https://api.dicebear.com/**"),
+		],
+	},
 };
 
 export default nextConfig;
