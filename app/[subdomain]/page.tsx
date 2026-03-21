d495799b6fae6e736fe53416f25ec24f9b07bff2
cuetrigg <imaginedoingwork@gmail.com>
2026-03-21 19:47:26 +0200

    feat(page): fit leaderboard pagination layout

--- app/[subdomain]/page.tsx
+++ app/[subdomain]/page.tsx
@@ -26,41 +26,33 @@ export default async function SubdomainPage({ params }: {
         <div className="w-full">
             <div className="flex flex-col w-full mb-10 sm:flex-row">
                 <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
-                    <div className="relative h-full ml-0 mr-0 sm:mr-10">
+                    <div className="relative ml-0 mr-0 sm:mr-10 h-fit">
                         <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-primary rounded-lg"></span>
                         <div className="relative h-full p-5 bg-white border-2 border-primary rounded-lg">
                             <EmployeeTable data={data} />
                         </div>
                     </div>
-                </div>
-                <div className="w-full h-96 sm:w-1/2">
-                    <div className="relative h-full ml-0 md:mr-10">
-                        <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-secondary rounded-lg"></span>
-                        <div className="relative h-full p-5 bg-white border-2 border-secondary rounded-lg overflow-y-auto">
-                            <BusinessUnitTable data={data} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {
-                data.userSettings.custom_employee_category_required &&
-                (
-                    <div className="flex flex-col w-full mb-5 sm:flex-row">
-                        <div className="flex-1">
-                        </div>
-                        <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
-                            <div className="relative h-full ml-0 mr-0 sm:mr-10">
+                    {
+                        data.userSettings.custom_employee_category_required &&
+                        (
+                            <div className="relative my-10 h-fit ml-0 mr-0 sm:mr-10">
                                 <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 rounded-lg bg-gradient-to-r from-primary to-secondary"></span>
                                 <div className="relative h-full p-5 bg-white border-2 border-primary rounded-lg">
                             	    <CustomCategoryTable data={data} />
                                 </div>
                             </div>
+                        )
+                    }
+                </div>
+                <div className="w-full sm:w-1/2">
+                    <div className="relative h-fit ml-0 md:mr-10">
+                        <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-secondary rounded-lg"></span>
+                        <div className="relative h-full p-5 bg-white border-2 border-secondary rounded-lg">
+                            <BusinessUnitTable data={data} />
                         </div>
-                        <div className="flex-1">
-                        </div>
-                    </div>
-                )
-            }
+                    </div>                    
+                </div>
+            </div>
         </div>
     </div>
     </>
