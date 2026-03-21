1a8c612fe5f802d615c2d1afe5c33ce7a95cfa25
cuetrigg <imaginedoingwork@gmail.com>
2026-03-21 21:57:19 +0200

    feat(business-units): pass units to client selector

--- components/tables/businessunitstable.tsx
+++ components/tables/businessunitstable.tsx
@@ -17,23 +17,11 @@ export default async function BusinessUnitTable({ data }: { data: AppConfig }) {
 	}
 
 	const businessunits: BusinessUnits = await response.json();
-	const businessUnit = businessunits[0];
-	const members = businessUnit?.members;
 
 	return (
-		<div>
-			<div className="flex items-center -mt-1">
-				<h3 className="my-2 ml-3 text-base font-bold text-gray-800">
-					Business Unit: {businessUnit?.name ?? "Unavailable"}
-				</h3>
-			</div>
-			<p className="mb-1 text-xs font-medium text-secondary uppercase">
-				------------
-			</p>
-			<BusinessUnitsTableClient
-				members={members}
-				pageSize={data.leaderBoardSettings.topVolunteersLimit}
-			/>
-		</div>
+		<BusinessUnitsTableClient
+			businessUnits={businessunits}
+			pageSize={data.leaderBoardSettings.topVolunteersLimit}
+		/>
 	);
 }
